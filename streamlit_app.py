709adca3df9259dfbf1aeced86b53c6946478c9d
Ancy252 <alvinancy11@gmail.com>
2024-08-03 20:41:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import Image
 
 # Define a function to load the model and cache it
 @st.cache_resource
@@ -30,28 +30,21 @@ if uploaded_file is not None:
     try:
         image = Image.open(uploaded_file)
         image = image.resize((256, 256))
-        image = ImageOps.expand(image, border=10, fill='black')  # Add a dark border
         img_array = np.array(image) / 255.0
         img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
 
-        # Center the image
-        col1, col2, col3 = st.columns([1, 2, 1])
-        with col1:
-            st.write("")
-        with col2:
-            st.image(image, caption='Uploaded Image.', use_column_width=True)
-        with col3:
-            st.write("")
-
+        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+        
         # Make prediction
         try:
             predictions = model.predict(img_array)
+            st.write(f"Predictions: {predictions}")
             predicted_class = np.argmax(predictions[0])
+            st.write(f"Predicted Class Index: {predicted_class}")
             predicted_label = categories[predicted_class]
 
-            # Display prediction results
-            st.markdown(f"<h3 style='text-align: center;'>Prediction: {predicted_label}</h3>", unsafe_allow_html=True)
-            st.markdown(f"<h4 style='text-align: center;'>Confidence: {predictions[0][predicted_class]:.2f}</h4>", unsafe_allow_html=True)
+            st.write(f'Prediction: {predicted_label}')
+            st.write(f'Confidence: {predictions[0][predicted_class]:.2f}')
         except Exception as e:
             st.error(f"Error during prediction: {e}")
 
